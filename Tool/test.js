10ebce16303918d21e5d75a9d205dee9c4fd32a1
周春光 <z_cg@foxmail.com>
2023-02-16 15:58:30 +0800

    translate chapter 7

--- Tool/test.js
+++
@@ -1,6 +0,0 @@
-const updateUserCache = (cache, { data: { addFakeUsers } }) => {
-  let data = cache.readQuery({ query: ROOT_QUERY });
-  data.totalUsers += addFakeUsers.length;
-  data.allUsers = [...data.allUsers, ...addFakeUsers];
-  cache.writeQuery({ query: ROOT_QUERY, data });
-};
